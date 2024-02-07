700f7a5b9169b1178d304b1117b092ba75ca99a1
nurullah ates <84018482+atesnrllh@users.noreply.github.com>
2024-02-07 16:23:07 +0300

    Update read_all_write_some_without_loading_entire_file_into_memory.py

--- read_all_write_some_without_loading_entire_file_into_memory.py
+++ read_all_write_some_without_loading_entire_file_into_memory.py
@@ -1,7 +1,7 @@
 
 input_file_path = 'write your input file path'
 output_file_path = 'write your output file path'
-with open(file_path) as old, open(output_file_path, 'w') as new:
-    for line in old:
+with open(file_path) as input, open(output_file_path, 'w') as output:
+    for line in input:
         if line.split('\t')[0] == '1':
-            new.write(line)
+            output.write(line)
