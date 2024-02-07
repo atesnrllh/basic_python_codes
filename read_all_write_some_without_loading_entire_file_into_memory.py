ea49b29f01cea74a417b845567737de875ec5595
nurullah ates <84018482+atesnrllh@users.noreply.github.com>
2024-02-07 16:22:21 +0300

    Update read_all_write_some_without_loading_entire_file_into_memory.py

--- read_all_write_some_without_loading_entire_file_into_memory.py
+++ read_all_write_some_without_loading_entire_file_into_memory.py
@@ -1,23 +1,7 @@
 
-file_path = '/home/nuri/Downloads/HEXA-main_fine/x_small/dev.txt'
-# with open(file_path, "r") as file1:
-#     with open("file2.txt", "w") as file2:
-#         x= file1.read()
-#         print()
-
-# import fileinput
-#
-# with open("dev.pos.txt", "w") as file_out:
-#     for line in fileinput.input(file_path, inplace=True):
-#         line_to_list = line.split('\t')
-#         if line_to_list[0] == "1":
-#             file_out.write(line)
-#         print()
-
-
-with open(file_path) as old, open('newtest', 'w') as new:
+input_file_path = 'write your input file path'
+output_file_path = 'write your output file path'
+with open(file_path) as old, open(output_file_path, 'w') as new:
     for line in old:
         if line.split('\t')[0] == '1':
             new.write(line)
-
-print()
